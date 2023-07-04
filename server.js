f569f258b0bf1f814262e9d32ffd4c862a1cd189
VikasHBishnoi <vikasbishnoi992858@gmail.com>
2023-07-04 21:24:07 +0530

    npm package rectify

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import morgan from 'morgan';
 import connectDb from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 // configure env
 dotenv.config();
@@ -16,6 +17,8 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
+// routes
+app.use('/api/v1/auth/',authRoutes);
 // Api
 app.get("/", (req, res) => {
     res.send(
