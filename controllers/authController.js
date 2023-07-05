4d2766e0a8374096150f5faf2a073db2ec05e238
VikasHBishnoi <vikasbishnoi992858@gmail.com>
2023-07-05 10:26:34 +0530

    Login backend complete

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
 
 export const registerController = async (req, res) => {
@@ -54,3 +54,50 @@ export const registerController = async (req, res) => {
         })
     }
 }
+
+// Post Login
+export const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        // Validation
+        if (!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: "Invalid email or pasword"
+            })
+        }
+
+        // checking user Exist or not
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "Email is not registered"
+            })
+        }
+
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res.status(404).send({
+                success: false,
+                message: "check creditential"
+            })
+        }
+
+        // token
+        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+        res.status(200).send({
+            success: "True",
+            message: "Sucessful login",
+            token
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: "Error in login",
+            err
+        })
+    }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import { loginController, registerController } from "../controllers/authController.js";
 // routers object
 const routers = express.Router();
 
@@ -15,7 +15,13 @@ const routers = express.Router();
 //     "address": "Jodhpur",
 //     "role": 0
 // }
-routers.post('/register', registerController)
+routers.post('/register', registerController);
 
+// To test login use below in postman
+// {
+//     "email":"test@mail.com",
+//     "password":"test@password"
+// }
+routers.post('/login',loginController);
 
 export default routers;
