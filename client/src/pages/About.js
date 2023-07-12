92ced399355d4179aba1811bab7adc7741f2572a
VikasHBishnoi <vikasbishnoi992858@gmail.com>
2023-07-12 16:43:01 +0530

    About Us Made

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from "react";
+import Layout from "../components/Layout/Layout";
 
 const About = () => {
-    return (
-        <Layout>
-            <h1>About Us</h1>
-        </Layout>
-    );
-}
+  return (
+    <Layout>
+      <div className="row contactus ">
+        <div className="col-md-4 ">
+          <img
+            src="/images/AboutUs.jpg"
+            alt="about us"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify">
+            <h1>Welcome to Our Ecommerce App!  </h1>
+            <p>At Our Ecommerce App, we believe in
+            bringing convenience and joy to your online shopping experience.
+            With a vast selection of products, seamless navigation, and
+            excellent customer service, we strive to be your go-to destination
+            for all your shopping needs.</p>
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default About
+export default About;
